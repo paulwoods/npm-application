ca31f900facf66e812065038cdeadf796b433416
Paul Woods <mr.paul.woods@gmail.com>
2024-06-01 23:59:50 -0500

    feat: dark/light working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {Box, Button, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import {Input, Label, Button, Stat} from "mui-component-library-mrpaulwoods";
+import {CssBaseline} from "@mui/material";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     const darkTheme = createTheme({
         palette: {
-            mode: 'dark',
+            mode: 'light',
         },
     });
 
@@ -17,46 +15,12 @@ function App() {
         <>
             <ThemeProvider theme={darkTheme}>
                 <CssBaseline/>
-
-                <Box display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center"
-                     gap="1em">
-                    <Button variant="outlined" color="primary">primary</Button>
-                    <Button variant="outlined" color="secondary">secondary</Button>
-                    <Button variant="outlined" color="success">success</Button>
-                    <Button variant="outlined" color="error">error</Button>
-                    <Button variant="outlined" color="info">info</Button>
-                    <Button variant="outlined" color="warning">warning</Button>
-                </Box>
-                <Box display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center"
-                     gap="1em">
-                    <Button variant="text" color="primary">primary</Button>
-                    <Button variant="text" color="secondary">secondary</Button>
-                    <Button variant="text" color="success">success</Button>
-                    <Button variant="text" color="error">error</Button>
-                    <Button variant="text" color="info">info</Button>
-                    <Button variant="text" color="warning">warning</Button>
-                </Box>
-
-                <div>
-                    <a href="https://vitejs.dev" target="_blank">
-                        <img src={viteLogo} className="logo" alt="Vite logo"/>
-                    </a>
-                    <a href="https://react.dev" target="_blank">
-                        <img src={reactLogo} className="logo react" alt="React logo"/>
-                    </a>
+                <h1>application: {darkTheme.palette.mode} mode</h1>
+                <Stat value={'55,555'} unit={'Unique Visitors Every Month'} />
+                <div style={{display:"flex", flexDirection:"column", gap: "1em"}}><Label>The Label</Label>
+                    <Input></Input>
+                    <Button>The Button</Button>
                 </div>
-                <h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
             </ThemeProvider>
         </>
     )
